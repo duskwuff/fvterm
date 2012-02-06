467219fefb602e5b19b3f3a01c09f79a403cf3bb
DusK <dusk@woofle.net>
2012-02-06 09:31:49 -0800

    global SIGCHLD handling (ignoring)

--- src/app/main.m
+++ src/app/main.m
@@ -1,4 +1,9 @@
+#import <signal.h>
+
 int main(int argc, char *argv[])
 {
+    // We don't care about our subprocesses exiting.
+    signal(SIGCHLD, SIG_IGN);
+
     return NSApplicationMain(argc, (const char **)argv);
 }
